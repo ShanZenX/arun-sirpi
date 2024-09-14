074d4d4a6ce7a0174e628914b4157adc2ccfa849
shanmugarajsekar <shanmugaraj2003@gmail.com>
2024-09-14 15:16:25 +0530

    error cleared

--- app/works/page.js
+++ app/works/page.js
@@ -1,4 +1,5 @@
 // work page
+"use client"
 import React from 'react'
 import Banner from '../Components/MIniComponents/Banner'
 import  img1  from '@/public/slider-4.jpg';
@@ -8,7 +9,7 @@ import PhotoModule from '../Components/MIniComponents/PhotoModule';
 export default function page() {
   return (
     <div>
-<Banner  img={img} title={"Our Work's"}/>
+<Banner  img={img1} title={"Our Work's"}/>
 <div className="flex flex-wrap w-10/12 justify-evenly my-10">
         <Image.PreviewGroup
           preview={{
